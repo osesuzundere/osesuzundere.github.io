0342c5da281ce2ea30827cc2694bcbd65f07b3ab
mujdecisy <mujdecisy@gmail.com>
2020-07-11 10:51:25 +0300

    init

---
+++ precache-manifest.d987aad27c0b58bc018607fbc61fdcb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87416de57a2e0fc2af3bdf605324ac1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f836847e6de71084d0",
+    "url": "/static/css/main.e3d40b40.chunk.css"
+  },
+  {
+    "revision": "8f79906c2d61ef4cdb19",
+    "url": "/static/js/2.ec3a6cfd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ec3a6cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f836847e6de71084d0",
+    "url": "/static/js/main.a17f539d.chunk.js"
+  },
+  {
+    "revision": "3d2b41aa967e9a9894d5",
+    "url": "/static/js/runtime-main.420bfc5e.js"
+  }
+]);
